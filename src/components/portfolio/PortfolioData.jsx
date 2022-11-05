b53b5158c7868937d2e15f6956def1b0864c0548
Kaif <sindhim@msoe.edu>
2022-11-05 12:49:34 -0500

    tabs

--- src/components/portfolio/PortfolioData.jsx
+++ src/components/portfolio/PortfolioData.jsx
@@ -17,242 +17,238 @@ import { RiReactjsFill } from "react-icons/ri";
 const PortfolioData = [
   {
     title: "Insight",
-    dates: (
-      <>
-        <p>
-          <i>Sept 2022 - Present</i>
-        </p>
-      </>
-    ),
-    summary: (
-      <>
-        <b>SUMMARY</b>
-        <p>
-          Insight is an in-progress senior design team project that I am
-          currently working on. It is a configuration web application in which
-          our product owner{" "}
-          <a target="__blank" href="https://www.newworldnow.com/">
-            New World Now
-          </a>{" "}
-          can deploy settings for its clients through one interface.
-        </p>
-      </>
-    ),
-    moreInfo: <p></p>,
-    technologies: (
-      <>
-        <b>TECH STACK</b>
-        <div className="technology">
-          <a
-            // href="https://material.angular.io/"
-            target="__blank"
-            className="technology_icon_holder"
-          >
-            {/* <div className="tooltip">Material UI</div> */}
-            <SiMaterialui className="icon" />
-            <SiMaterialui className="icon hover" />
-          </a>
-          <a
-            // href="https://angular.io/"
-            target="__blank"
-            className="technology_icon_holder"
-          >
-            <RiAngularjsFill className="icon" />
-            <RiAngularjsLine className="icon hover" />
-          </a>
-          <a
-            // href="https://learn.microsoft.com/en-us/dotnet/csharp/"
-            target="__blank"
-            className="technology_icon_holder"
-          >
-            <TbCSharp className="icon" />
-            <SiCsharp className="icon hover" />
-          </a>
-          <a
-            // href="https://www.mongodb.com/"
-            target="__blank"
-            className="technology_icon_holder"
-          >
-            <DiMongodb className="icon" />
-            <SiMongodb className="icon hover" />
-          </a>
-        </div>
-      </>
-    ),
+    pages: [
+      {
+        title: "Summary",
+        content: (
+          <>
+            <p>
+              <i>Sept 2022 - Present</i>
+            </p>
+            <b>SUMMARY</b>
+            <p>
+              Insight is an in-progress senior design team project that I am
+              currently working on. It is a configuration web application in
+              which our product owner{" "}
+              <a target="__blank" href="https://www.newworldnow.com/">
+                New World Now
+              </a>{" "}
+              can deploy settings for its clients through one interface.
+            </p>
+            <br />
+            <b>TECH STACK</b>
+            <div className="technology">
+              <a
+                // href="https://material.angular.io/"
+                target="__blank"
+                className="technology_icon_holder"
+              >
+                {/* <div className="tooltip">Material UI</div> */}
+                <SiMaterialui className="icon" />
+                <SiMaterialui className="icon hover" />
+              </a>
+              <a
+                // href="https://angular.io/"
+                target="__blank"
+                className="technology_icon_holder"
+              >
+                <RiAngularjsFill className="icon" />
+                <RiAngularjsLine className="icon hover" />
+              </a>
+              <a
+                // href="https://learn.microsoft.com/en-us/dotnet/csharp/"
+                target="__blank"
+                className="technology_icon_holder"
+              >
+                <TbCSharp className="icon" />
+                <SiCsharp className="icon hover" />
+              </a>
+              <a
+                // href="https://www.mongodb.com/"
+                target="__blank"
+                className="technology_icon_holder"
+              >
+                <DiMongodb className="icon" />
+                <SiMongodb className="icon hover" />
+              </a>
+            </div>
+          </>
+        ),
+      },
+      {
+        title: "Process",
+      },
+    ],
   },
   {
     title: "Board View",
-    dates: (
-      <>
-        <p>
-          <i>August 2022</i>
-        </p>
-      </>
-    ),
-    summary: (
-      <>
-        <b>SUMMARY</b>
-        <p>
-          Board View was an investigative full-stack personal project into
-          remaking an existing web application Trello.
-        </p>
-      </>
-    ),
-    moreInfo: <p></p>,
-    technologies: (
-      <>
-        <b>TECH STACK</b>
-        <div className="technology">
-          <a
-            // href=""
-            target="__blank"
-            className="technology_icon_holder"
-          >
-            <RiReactjsFill className="icon" />
-            <RiReactjsFill className="icon hover" />
-          </a>
-          <a
-            // href=""
-            target="__blank"
-            className="technology_icon_holder"
-          >
-            <SiPython className="icon" />
-            <SiPython className="icon hover" />
-          </a>
-          <a
-            // href=""
-            target="__blank"
-            className="technology_icon_holder"
-          >
-            <DiDjango className="icon" />
-            <DiDjango className="icon hover" />
-          </a>
-          <a
-            // href=""
-            target="__blank"
-            className="technology_icon_holder"
-          >
-            <DiSqllite className="icon" />
-            <DiSqllite className="icon hover" />
-          </a>
-        </div>
-      </>
-    ),
+    pages: [
+      {
+        title: "Summary",
+        content: (
+          <>
+            <p>
+              <i>August 2022</i>
+            </p>
+            <b>SUMMARY</b>
+            <p>
+              Board View was an investigative full-stack personal project into
+              remaking an existing web application Trello.
+            </p>
+            <br />
+            <b>TECH STACK</b>
+            <div className="technology">
+              <a
+                // href=""
+                target="__blank"
+                className="technology_icon_holder"
+              >
+                <RiReactjsFill className="icon" />
+                <RiReactjsFill className="icon hover" />
+              </a>
+              <a
+                // href=""
+                target="__blank"
+                className="technology_icon_holder"
+              >
+                <SiPython className="icon" />
+                <SiPython className="icon hover" />
+              </a>
+              <a
+                // href=""
+                target="__blank"
+                className="technology_icon_holder"
+              >
+                <DiDjango className="icon" />
+                <DiDjango className="icon hover" />
+              </a>
+              <a
+                // href=""
+                target="__blank"
+                className="technology_icon_holder"
+              >
+                <DiSqllite className="icon" />
+                <DiSqllite className="icon hover" />
+              </a>
+            </div>
+          </>
+        ),
+      },
+      { title: "Process" },
+    ],
   },
   {
     title: "Upcycle",
-    dates: (
-      <>
-        <p>
-          <i>Sept 2021 - May 2022</i>
-        </p>
-      </>
-    ),
-    summary: (
-      <>
-        <b>WHAT IS UPCYCLE?</b>
-        <p>
-          Upcycle is a social network in which schools in the Milwaukee area can
-          offer, request, and transfer resources they may have. It was a
-          year-long junior design team project.
-        </p>
-      </>
-    ),
-    moreInfo: (
-      <>
-        <b>WHAT WAS THE GOAL?</b>
-        <p>
-          Upcycle was a continuing project from a previous junior design team
-          but did not have a social feature nor was a web app. In order to do
-          migrate it to a web app, we used an open-source toolkit called
-          Bonfire.
-        </p>
-        <br />
-        <b>
-          WHAT IS{" "}
-          <a
-            className="link"
-            target="__blank"
-            href="https://bonfirenetworks.org/"
-          >
-            BONFIRE
-          </a>
-          ?
-        </b>
-        <p>
-          Bonfire is an open-source toolkit which comes ready with social
-          features. It operates with sets of configurations called "flavours."
-          You can create your own flavour in Bonfire to add additional
-          implementation.
-        </p>
-        <br />
-        <b>WHAT DID WE DO?</b>
-        <p>
-          We worked closely with the Bonfire developers and our product owners
-          to understand the source code. Once we were familiar with it, we
-          created our own flavour called "upcycle," which is viewable and
-          runnable from Bonfire's repo{" "}
-          <a
-            target="__blank"
-            href="https://github.com/bonfire-networks/bonfire-app/tree/main/flavours/upcycle"
-          >
-            here
-          </a>
-          .
-        </p>
-        <br />
-        <b>WHAT WAS MY ROLE?</b>
-        <p>
-          My work on the project primarily consisted of doing frontend related
-          tasks. That included creating, styling, and implementing UI mockups,
-          dynamic page generation, and API request handling.
-        </p>
-      </>
-    ),
-    technologies: (
-      <>
-        <b>TECH STACK</b>
-        <div className="technology">
-          <a
-            href="https://hexdocs.pm/phoenix_live_view/Phoenix.LiveView.html"
-            target="__blank"
-            className="technology_icon_holder"
-          >
-            <FaPhoenixFramework className="icon" />
-            <FaPhoenixFramework className="icon hover" />
-          </a>
-          <a
-            // href="https://elixir-lang.org/"
-            target="__blank"
-            className="technology_icon_holder"
-          >
-            <SiElixir className="icon" />
-            <SiElixir className="icon hover" />
-          </a>
-          <a
-            // href="https://graphql.org/"
-            target="__blank"
-            className="technology_icon_holder"
-          >
-            <SiGraphql className="icon" />
-            <SiGraphql className="icon hover" />
-          </a>
-          <a
-            // href="https://www.docker.com/"
-            target="__blank"
-            className="technology_icon_holder"
-          >
-            <SiDocker className="icon" />
-            <SiDocker className="icon hover" />
-          </a>
-        </div>
-      </>
-    ),
+    pages: [
+      {
+        title: "Summary",
+        content: (
+          <>
+            <p>
+              <i>Sept 2021 - May 2022</i>
+            </p>
+            <b>WHAT IS UPCYCLE?</b>
+            <p>
+              Upcycle is a social network in which schools in the Milwaukee area
+              can offer, request, and transfer resources they may have. It was a
+              year-long junior design team project.
+            </p>
+            <br />
+            <b>TECH STACK</b>
+            <div className="technology">
+              <a
+                href="https://hexdocs.pm/phoenix_live_view/Phoenix.LiveView.html"
+                target="__blank"
+                className="technology_icon_holder"
+              >
+                <FaPhoenixFramework className="icon" />
+                <FaPhoenixFramework className="icon hover" />
+              </a>
+              <a
+                // href="https://elixir-lang.org/"
+                target="__blank"
+                className="technology_icon_holder"
+              >
+                <SiElixir className="icon" />
+                <SiElixir className="icon hover" />
+              </a>
+              <a
+                // href="https://graphql.org/"
+                target="__blank"
+                className="technology_icon_holder"
+              >
+                <SiGraphql className="icon" />
+                <SiGraphql className="icon hover" />
+              </a>
+              <a
+                // href="https://www.docker.com/"
+                target="__blank"
+                className="technology_icon_holder"
+              >
+                <SiDocker className="icon" />
+                <SiDocker className="icon hover" />
+              </a>
+            </div>
+            <br />
+            <b>WHAT WAS THE GOAL?</b>
+            <p>
+              Upcycle was a continuing project from a previous junior design
+              team but did not have a social feature nor was a web app. In order
+              to do migrate it to a web app, we used an open-source toolkit
+              called Bonfire.
+            </p>
+            <br />
+            <b>
+              WHAT IS{" "}
+              <a
+                className="link"
+                target="__blank"
+                href="https://bonfirenetworks.org/"
+              >
+                BONFIRE
+              </a>
+              ?
+            </b>
+            <p>
+              Bonfire is an open-source toolkit which comes ready with social
+              features. It operates with sets of configurations called
+              "flavours." You can create your own flavour in Bonfire to add
+              additional implementation.
+            </p>
+            <br />
+            <b>WHAT DID WE DO?</b>
+            <p>
+              We worked closely with the Bonfire developers and our product
+              owners to understand the source code. Once we were familiar with
+              it, we created our own flavour called "upcycle," which is viewable
+              and runnable from Bonfire's repo{" "}
+              <a
+                target="__blank"
+                href="https://github.com/bonfire-networks/bonfire-app/tree/main/flavours/upcycle"
+              >
+                here
+              </a>
+              .
+            </p>
+            <br />
+            <b>WHAT WAS MY ROLE?</b>
+            <p>
+              My work on the project primarily consisted of doing frontend
+              related tasks. That included creating, styling, and implementing
+              UI mockups, dynamic page generation, and API request handling.
+            </p>
+          </>
+        ),
+      },
+      { title: "Process" },
+      { title: "Analysis" },
+    ],
   },
 ];
 
 export const Selection = () => {
-  const [selection, setSelection] = useState(PortfolioData.at(0));
+  const [project, setProject] = useState(PortfolioData.at(0));
+  const [projectTab, setProjectTab] = useState(project.pages.at(0));
 
   return (
     <>
@@ -261,12 +257,13 @@ export const Selection = () => {
           return (
             <div
               className={
-                selection.title === button.title
+                project.title === button.title
                   ? "btn btn-secondary option selected"
                   : "btn btn-secondary option"
               }
               onClick={() => {
-                setSelection(button);
+                setProject(button);
+                setProjectTab(button.pages.at(0));
               }}
             >
               {button.title}
@@ -274,14 +271,44 @@ export const Selection = () => {
           );
         })}
       </div>
+      <div className="portfolio_divider vertical" />
       <div className="container portfolio_description">
-        <div>
-          {selection.dates}
-          <br />
-          {selection.summary}
-          {selection.technologies}
-          <br />
-          {selection.moreInfo}
+        <div className="tabs_container">
+          {project.pages.length === 1 ? (
+            <div className="btn btn-secondary tab">
+              {project.pages.at(0).title}
+            </div>
+          ) : (
+            project.pages.map((page, index) => {
+              var position = null;
+              if (index === 0) {
+                position = "first";
+              } else if (index === project.pages.length - 1) {
+                position = "last";
+              } else {
+                position = "mid";
+              }
+              return (
+                <>
+                  <div
+                    onClick={() => setProjectTab(page)}
+                    className={
+                      "btn btn-secondary " +
+                      position +
+                      " tab" +
+                      (projectTab.title === page.title ? " selected" : "")
+                    }
+                  >
+                    {page.title}
+                  </div>
+                </>
+              );
+            })
+          )}
+        </div>
+        <br />
+        <div className="content">
+          {projectTab.content}
           <br />
         </div>
       </div>
